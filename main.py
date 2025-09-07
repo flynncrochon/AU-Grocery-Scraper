de716782c7dba4a02b4a5d0580d2882f2e52afc9
Flynn Crochon <flynncrochon@gmail.com>
2025-09-07 15:14:07 +1000

    update main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Coles_Scraper import Coles_Scraper
 
 # open a coles session using ssid as a sample proxy
+#
 ssid = "sample_ssid"
 coles_session = Coles_Scraper(ssid)
 coles_session.scrape_all_inner_categories()
